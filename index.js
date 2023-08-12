61786b3a117b6c7f1a121ae7806eda6dda4d2a43
imane omari <imane3.omari@gmail.com>
2023-08-12 01:22:15 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// index.js
+
 function addingEventListener() {
+  const button = document.getElementById("button");
+  const main = document.getElementById("main");
+
+  button.addEventListener("click", function () {
+    main.textContent = "Button was clicked!";
+  });
 }
+
+addingEventListener();
